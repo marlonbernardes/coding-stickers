a5376a79d5802e2e67e6e1d561c7f699f740b7dc
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-06 21:00:09 -0200

    Add chai-immutable & create setup file for tests

---
+++ client/test/setup.js
@@ -0,0 +1,9 @@
+import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
+
+chai.use(chaiImmutable);
