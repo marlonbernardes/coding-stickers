fca88eaa0e934187aec451f1937b1cbff45cb4da
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-06 19:42:36 -0200

    Add eslint support

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router, { Route } from 'react-router';
+import Router from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducer';
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,5 +1,5 @@
 import { Map } from 'immutable';
 
-export default function(state = Map(), action) {
+export default function (state = new Map()) {
   return state;
 }
