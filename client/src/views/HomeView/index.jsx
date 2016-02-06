e2f94aaad5cdc2257893d341edea9e2d118965e9
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-06 21:39:24 -0200

    Use CustomizationWidget inside of HomeView

--- client/src/views/HomeView/index.jsx
+++ client/src/views/HomeView/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import CustomizationWidget from '../../components/CustomizationWidget';
 
-export default class Home extends Component {
+export default class HomeView extends Component {
 
   render() {
     return (
       <div>
         <h1>home view</h1>
+        <CustomizationWidget />
       </div>
     );
   }
