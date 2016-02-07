79b4b7f247abf5aa6b49de71bdbd581da6310893
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-06 22:32:31 -0200

    Make mocha ignore styles during tests

--- client/test/setup.js
+++ client/test/setup.js
@@ -1,6 +1,7 @@
 import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
+import ignoreStyles from 'ignore-styles';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
