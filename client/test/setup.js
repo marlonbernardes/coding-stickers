4d86aeb3dd0e8d92cce02901cc37c3b3078b11d9
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-11 17:01:09 -0200

    Add spies for Chai assertion library

--- client/test/setup.js
+++ client/test/setup.js
@@ -1,10 +1,12 @@
 import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
+import chaiSpies from 'chai-spies';
 import ignoreStyles from 'ignore-styles';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
 
+chai.use(chaiSpies);
 chai.use(chaiImmutable);
