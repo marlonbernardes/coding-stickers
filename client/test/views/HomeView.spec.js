239efe3c1b4836af4e198ee9b156b3f650c95b75
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-12 02:33:11 -0200

    Add missing tests for HomeView

---
+++ client/test/views/HomeView.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import HomeView from '../../src/views/HomeView';
+import CustomizationContainer from '../../src/containers/CustomizationContainer';
+import PageHeader from '../../src/components/PageHeader';
+
+describe('<HomeView />', () => {
+
+  it('renders the customization container', () => {
+    const component = shallow(<HomeView />);
+    expect(component.find(CustomizationContainer)).to.have.length(1);
+  });
+
+  it('renders the page header', () => {
+    const component = shallow(<HomeView />);
+    expect(component.find(PageHeader)).to.have.length(1);
+  });
+
+});
