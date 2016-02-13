e300e03778e373038e2c8d6f6a9a19b7b6d5bc85
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-13 01:02:44 -0200

    Fix incomplete test name

--- client/test/api/stickers.spec.js
+++ client/test/api/stickers.spec.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 import StickersApi from '../../src/api/stickers';
 
 describe('Stickers API', () => {
-  it('performs ', () => {
+  it('performs a GET request to stickers_data.json', () => {
     const spy = chai.spy.on(axios, 'get');
-    StickersApi.findStickers('ee');
+    StickersApi.findStickers('query');
     expect(spy).to.have.been.called.with('/sticker_data.json');
   });
 });
