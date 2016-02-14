265af9dfb64b2e23501a3921db211152d86d9d75
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-14 18:10:58 -0200

    Move StickerMule logo to the footer

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './Footer.module.scss';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <a className={styles.link} href="https://www.stickermule.com">
+          <span className={styles.text}>Stickers by</span> 
+          <img className={styles.image} src="/img/general/logo.svg"/>
+        </a>
+      </div>
+    );
+  }
+}
