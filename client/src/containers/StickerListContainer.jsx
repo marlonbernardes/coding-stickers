268c274e734e40e5055e08f20a6aa22e854f9426
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-02-14 13:56:34 -0200

    Wrap sticker list inside its own container

---
+++ client/src/containers/StickerListContainer.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import StickerList from '../components/StickerList';
+
+export class StickerListContainer extends Component {
+
+  render() {
+    return (
+      <StickerList
+        onClickSticker={this.props.addCustomization}
+        stickers={this.props.stickers}
+      />
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    stickers: currentPageStickers(state),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addCustomization: (sticker) => {
+      dispatch({ type: 'ADD_CUSTOMIZATION', sticker });
+    },
+  };
+}
+
+function currentPageStickers(state) {
+  const { stickers, pagination } = state;
+  const page = pagination.get('current') - 1;
+  const perPage = pagination.get('perPage');
+  return stickers.slice(page * perPage, (page * perPage) + perPage);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(StickerListContainer);
